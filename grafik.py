9dbd7dc986b318a85b2fb6534cca5d498918f6a5
budykinaSV <budyki@mail.ru>
2016-07-31 22:00:34 +0500

    Черепашка рисует график функции

---
+++ grafik.py
@@ -0,0 +1,13 @@
+from proba import *
+from time import sleep
+def f(x):
+    return x**2
+pen_up()
+to_point(-5,f(-5))
+pen_down()
+for x in range(-5,6,1):
+    to_point(x,f(x))
+pen_up()
+
+sleep(10)
+
