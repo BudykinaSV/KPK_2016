0d76b832b3e4b003eeeb0a88cab5ef5840ed85d2
budykinaSV <budyki@mail.ru>
2016-08-03 23:37:43 +0500

    Игра в шарики

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -1,50 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import Tkinter #подключение библиотеки
+import Tkinter
+from random import choice, randint
+from math import cos, sin, pi
+import time
+import signal
 
-def button1_command():
-    print('Enter tuknut')
+ball_initial_number=100
+ball_maximal_radius=30
+ball_minimal_radius=15
+ball_available_colors=["lightgray","blue","red","#FFFF00","#FF00FF"]
+
+def click_ball(event):
+    """обработчик событий мыши, удаление шарика по клику мышкой
+    вести счет кликнутых шариков
+    """
+    #event.x, event.y
+    obj = canvas.find_closest(event.x, event.y)
+    x1,y1,x2,y2=canvas.coords(obj)
+    if x1<=event.x<=x2 and y1<=event.y<=y2:
+        canvas.delete(obj)
+
+def create_random_ball():
+    """создание шарика с случайным размещением
+    шарик не выходит за границы холста
+    """
+    R=randint(ball_minimal_radius,ball_maximal_radius)
+    x=randint(0+R,int(canvas['width'])-1-2*R)
+    y=randint(0+R,int(canvas['height'])-1-2*R)
+
+    oval=canvas.create_oval(x,y,x+2*R,y+2*R,fill=random_color(),width=1, outline="red")
 
 
-def print_hello(event):
-    print("Hello!")
-    #print(event.char)
-    #print(event.keycode)
-    #print(event.num)
-    print(event.x,event.y)
-    #print(event.x_root,event.y_root)
-    m1=event.widget
-    print(m1)
-    if m1==button1:
-        print("button1")
-    elif m1==button2:
-        print("button2")
-    else:
-        print("error")
-        #raise ValueError()
+def random_color():
+    """генератор случайных цветов шариков"""
+    return choice(ball_available_colors)
 
+def init_ball_catch_game():
+    """
+    Создает необходимое для игр количество шариков,
+    по которым нужно кликать
+    """
+    for i in range(ball_initial_number):
+        create_random_ball()
 
 def init_main_window():
-    global root, button1, button2, label, text, scale
+    global root, canvas
+
     root=Tkinter.Tk()
-    button1=Tkinter.Button(root,text='Button1',command=button1_command)
-    button1.bind('<Button>',print_hello)
-    button1.pack()
-    button2=Tkinter.Button(root,text='Button2')
-    button2.bind('<Button>',print_hello)
-    button2.pack()
 
-    variable=Tkinter.IntVar(0)
-    label=Tkinter.Label(root,textvariable=variable)
-    text=Tkinter.Entry(root, textvariable=variable)
-    scale=Tkinter.Scale(root,orient=Tkinter.HORIZONTAL, length=300,
-        from_=0, to=100, tickinterval=10, resolution=5, variable=variable)
-    for obj in label,text,scale:
-        obj.pack()
-
+    canvas=Tkinter.Canvas(root, background='green',width=400,height=400)
+    canvas.bind("<Button>", click_ball)
+    canvas.pack()
 
 
 if __name__=="__main__":
     init_main_window()
-
-root.mainloop()
+    init_ball_catch_game()
+    root.mainloop()
