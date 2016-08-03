14e71587477be401fdcb83c67ee795ffae1cb764
budykinaSV <budyki@mail.ru>
2016-08-03 20:57:52 +0500

    Эксперименты с Thinter_2

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -21,13 +21,27 @@ def print_hello(event):
         print("error")
         #raise ValueError()
 
-root=Tkinter.Tk()
 
-button1=Tkinter.Button(root,text='Button1',command=button1_command)
-button1.bind('<Button>',print_hello)
-button1.pack()
-button2=Tkinter.Button(root,text='Button2')
-button2.bind('<Button>',print_hello)
-button2.pack()
+def init_main_window():
+    global root, button1, button2, label, text, scale
+    root=Tkinter.Tk()
+    button1=Tkinter.Button(root,text='Button1',command=button1_command)
+    button1.bind('<Button>',print_hello)
+    button1.pack()
+    button2=Tkinter.Button(root,text='Button2')
+    button2.bind('<Button>',print_hello)
+    button2.pack()
+
+
+    label=Tkinter.Label(root,text='Some text')
+    text=Tkinter.Entry(root)
+    scale=Tkinter.Scale(root)
+    for obj in label,text,scale:
+        obj.pack()
+
+
+
+if __name__=="__main__":
+    init_main_window()
 
 root.mainloop()
