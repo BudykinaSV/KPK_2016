6b7e9c6605ed44eed3a850045d6ed19d8d2cb5dd
BudykinaSV <budyki@mail.ru>
2016-08-08 09:13:20 +0600

    Сделан класс шариков, after - задержка

--- the_gun/gun.py
+++ the_gun/gun.py
@@ -3,7 +3,7 @@ from random import choice, randint
 
 screen_width=400
 screen_height=400
-timer_delay=2000
+timer_delay=100
 
 class Ball:
     initial_number=10
@@ -26,13 +26,55 @@ class Ball:
         self._Vy=randint(-2,2)
 
 
+    def fly(self):
+        self._x+=self._Vx
+        self._y+=self._Vy
+        #отражение от стенок
+        if self._x<0:
+            self._x=0
+            self._Vx=-self._Vx
+        elif self._x+2*self._R>=screen_width:
+            self._x=screen_width-2*self._R-1
+            self._Vx=-self._Vx
+        if self._y<0:
+            self._y=0
+            self._Vy=-self._Vy
+        elif self._y+2*self._R>=screen_height:
+            self._y=screen_height-2*self._R-1
+            self._Vy=-self._Vy
+
+        canvas.coords(self._avatar,self._x,self._y,
+                      self._x+2*self._R,self._y+2*self._R)
+
+
+
+class Gun:
+    def __init__(self):
+        self._x = 0
+        self._y = screen_height-1
+        self._lx = +30
+        self._ly = -30
+        self._avatar = canvas.create_line(self._x, self._y,
+                                          self._x+self._lx,
+                                          self._y+self._ly)
+
+    def shoot(self):
+        """
+        :return возвращает объект снаряда (класса Shell)
+        """
+        shell = Shell(self._x + self._lx, self._y + self._ly,
+                      self._lx/10, self._ly/10)
+        return shell
+
 
 def init_game():
     """
     создание шариков и пушки
     """
-    global balls
+    global balls, gun, shells_on_fly
     balls=[Ball() for i in range(Ball.initial_number)]
+    gun = Gun() #создать экземпляр пушки
+    shells_on_fly = []
 
 
 def init_main_window():
@@ -44,10 +86,22 @@ def init_main_window():
     scores_text = Entry(root, textvariable=scores_value)
     canvas.grid(row=1, column=0, columnspan=3)
     scores_text.grid(row=0,column=2)
+    canvas.bind('<Button-1>', click_event_handler)
 
 def timer_event():
-    print('Hello')
+    #print('Tik-tak')
+    for ball in balls:
+        ball.fly()
+    for shell in shells_on_fly:
+        shell.fly()
+
+
     canvas.after(timer_delay, timer_event)#каждые 2сек пишем hello
+
+def click_event_handler(event):
+    global shells_on_fly
+    shell = gun.shoot()
+    shells_on_fly.append(shell)
 
 if __name__ == "__main__":
     init_main_window()
